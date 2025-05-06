a6a9fece15311b918082d17c1a8a8bf6ef7a386a
sfouziabanu <sfouziabanu2004@gmail.com>
2025-05-06 13:48:24 +0530

    updated test

--- sever.test.js
+++ sever.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./app'); 
+const app = require('./index'); 
 
 describe("API Tests", () => {
     it('GET /api/health should return status ok', async () => {
